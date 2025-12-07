008adc782a0c6febd8cfd0bc3bfe1fb6aa09ccbc
StackOdyssey <davidsmithny01@gmail.com>
2025-12-07 19:43:55 +0100

    Initial Django blog setup with models, templates, and migrations

---
+++ Alx_DjangoLearnLab/advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.conf import settings
+from datetime import date
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        permissions = [
+        ("can_view", "Can view book"),
+        ("can_create", "Can create book"),
+        ("can_edit", "Can edit book"),
+        ("can_delete", "Can delete book"),
+    ]
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return self.name
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+   
+
---
+++ django_blog/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html', {"posts" : posts})
+
+# Create your views here.
