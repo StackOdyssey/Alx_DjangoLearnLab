0630a80636552a4232bb3b8b8438d3ae8962695e
StackOdyssey <davidsmithny01@gmail.com>
2025-12-14 18:39:12 +0100

    Include all models, serializers, views, and URL configurations related to posts and comments.

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+    comments_count = serializers.IntegerField(source='comments.count', read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'author_username', 'title', 'content', 'comments_count', 'created_at', 'updated_at']
+        read_only_fields = ['author', 'author_username', 'comments_count', 'created_at', 'updated_at']
+    
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
+class CommentSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+    post_title = serializers.ReadOnlyField(source='post.title')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'post_title', 'author', 'author_username', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['author', 'author_username', 'post_title', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+
