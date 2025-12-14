77842e7787770968443e4d729966a6dee7c36751
StackOdyssey <davidsmithny01@gmail.com>
2025-12-14 18:55:50 +0100

    create notification app in the project and fixing some issues

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from .models import Post
 
 User = settings.AUTH_USER_MODEL
 
@@ -28,3 +29,16 @@ class Comment(models.Model):
     
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
+
+class Like(models.Model):
+    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.user} liked {self.post}'
+
