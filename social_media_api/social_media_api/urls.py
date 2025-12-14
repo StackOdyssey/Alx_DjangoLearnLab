422cf572202cba28916d05b730f1b47d0a5cc4b2
StackOdyssey <davidsmithny01@gmail.com>
2025-12-14 18:42:01 +0100

    adding API prefix in routes

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', include('posts.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
 
 ]
