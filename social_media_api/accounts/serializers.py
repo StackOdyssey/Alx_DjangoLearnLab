6f2e71a280521e1e40411e7a81bfe0b9316c5e1d
StackOdyssey <davidsmithny01@gmail.com>
2025-12-14 18:18:48 +0100

    creating social_media_api fixxing syntax errors

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,24 +1,29 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from .models import CustomUser
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture']
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        # Create user using get_user_model().objects.create_user
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
+        # Explicitly create token here
+        Token.objects.create(user=user)
         return user
 
 class LoginSerializer(serializers.Serializer):
